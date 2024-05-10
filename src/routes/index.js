277c23fcabea33cc5444da1a913bf06185c64628
Kirubel <meetkirubel@gmail.com>
2024-05-10 15:26:27 +0300

    first commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const db = require("../models/db");
 
 const route = express.Router();
-route.get("/", (req, res) => {
+route.get("/api/content", (req, res) => {
   db.promise()
     .query(`select * from content`)
     .then((data) => {
